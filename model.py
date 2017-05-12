f51053082f6b9b4f15e996fb28df00db934e5502
Alex Ramos <alex_ramos@apple.com>
2017-05-12 13:24:44 -0700

    Refactor model.py to use generators

--- model.py
+++ model.py
@@ -9,56 +9,74 @@ from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, Cropping2D
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
+from sklearn.model_selection import train_test_split
 
-# load data
+OFFCENTER_CAMERA_STEERING_CORRECTION = 0.2
+BATCH_SIZE = 32
 
+def gen_imgs(samples, batch_size):
+    '''Train '''
+    num_samples = len(samples)
+    while 1: # Loop forever so the generator never terminates
+        samples = samples.sample(frac=1) # shuffle samples
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples.iloc[offset:offset+batch_size]
+
+            images = []
+            angles = []
+            for _, batch_sample in batch_samples.iterrows():
+                img = cv2.cvtColor(cv2.imread(str(batch_sample.img_path)), cv2.COLOR_BGR2RGB)
+                angle = batch_sample.steering
+
+                if batch_sample.camera_position.startswith('left'):
+                    steering_correction = OFFCENTER_CAMERA_STEERING_CORRECTION
+                elif batch_sample.camera_position.startswith('right'):
+                    steering_correction = -OFFCENTER_CAMERA_STEERING_CORRECTION
+                else:
+                    steering_correction = 0.0
+
+                if batch_sample.flip_img:
+                    img = cv2.flip(img, 1)
+                    angle = angle * -1.0
+                    steering_correction  * -1.0
+
+                angle += steering_correction
+
+                images.append(img)
+                angles.append(angle)
+
+            X_train = np.array(images)
+            y_train = np.array(angles)
+            yield X_train, y_train
+
+# Load img paths and steering values
 input_dir = Path('data/IMG/')
 headers = ['center_image', 'left_image', 'right_image',
 	'steering', 'throttle', 'brake', 'speed']
-df = pd.read_csv('data/driving_log.csv', header=None, names=headers) #, skiprows=1)
-df.brake = 0
+samples = pd.read_csv('data/driving_log.csv', header=None, names=headers) #, skiprows=1)
 
+# Training images were generated on Windows machine, fix paths to work on Linux/MacOS machines
 fix_path = lambda p: input_dir / PureWindowsPath(p).parts[-1]
 for col in ['center_image', 'left_image', 'right_image']:
-	df[col]= df[col].apply(fix_path)
-    # df[col] = 'data/' + df[col]
+	samples[col]= samples[col].apply(fix_path)
 
-OFFCENTER_CORRECTION = 0.2
+# Drop unneccessary columns and reshape dataframe
+samples.drop(['throttle', 'brake', 'speed'], axis=1, inplace=True)
+samples = pd.melt(samples, id_vars=['steering'], value_vars=['center_image', 'left_image', 'right_image'],
+    var_name='camera_position', value_name='img_path')
+samples['camera_position'] = samples.camera_position.str.replace('_image', '')
 
-load_img = lambda p: cv2.cvtColor(cv2.imread(str(p)), cv2.COLOR_BGR2RGB)
+# Augment data with flipped images
+samples['flip_img'] = False # flag to let gen_imgs know whether to flip image 
+samples_flipped = samples.copy()
+samples['flip_img'] = True
+samples = pd.concat([samples, samples_flipped], ignore_index=True)
 
-images = list()
-measurements = list()
-print("Loading center images...")
-for i, img_path in enumerate(tqdm(df.center_image)):
-    img = load_img(img_path)
-    images.append(img)
-    measurements.append(df.steering.iloc[i])
-    # augment data with flipped images
-    images.append(cv2.flip(img, 1)) 
-    measurements.append(df.steering.iloc[i] * -1.0)
+train_samples, validation_samples = train_test_split(samples, test_size=0.2)
+train_generator = gen_imgs(train_samples, batch_size=BATCH_SIZE)
+validation_generator = gen_imgs(validation_samples, batch_size=BATCH_SIZE)
 
-print("Loading left images...")
-for i, img_path in enumerate(tqdm(df.left_image)):
-    img = load_img(img_path)
-    images.append(img)
-    measurements.append(df.steering.iloc[i] + OFFCENTER_CORRECTION)
-    # augment data with flipped images
-    images.append(cv2.flip(img, 1)) 
-    measurements.append(df.steering.iloc[i] * -1.0 - OFFCENTER_CORRECTION)
-
-print("Loading right images...")
-for i, img_path in enumerate(tqdm(df.right_image)):
-    img = load_img(img_path)
-    images.append(img)
-    measurements.append(df.steering.iloc[i] - OFFCENTER_CORRECTION)
-    # augment data with flipped images
-    images.append(cv2.flip(img, 1)) 
-    measurements.append(df.steering.iloc[i] * -1.0 + OFFCENTER_CORRECTION)
-
-X_train = np.array(images)
-y_train = np.array(measurements)
-
+# Define network architecture
 model = Sequential()
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
 model.add(Cropping2D(cropping=((70, 25), (0,0))))
@@ -74,26 +92,10 @@ model.add(Dense(10))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2,
-    shuffle=True, epochs=3)
+
+model.fit_generator(train_generator, np.ceil(len(train_samples) / BATCH_SIZE),
+    validation_data=validation_generator,
+    validation_steps=np.ceil(len(validation_samples) / BATCH_SIZE),
+    epochs=3)
 
 model.save('model.h5')
-
-# preprocess
-# img = cv2.cvtColor(cv2.imread(img_path), cv2.COLOR_BGR2RGB)
-
-# plt.imshow(img)
-# plt.show()
-
-# - crop
-# - resize?
-# - change colors
-
-# create model
-# - use nvida architecture
-# - model with keras
-
-# train model
- 
-# save model to h5 file
-# check out https://keras.io/getting-started/faq/#how-can-i-save-a-keras-model
