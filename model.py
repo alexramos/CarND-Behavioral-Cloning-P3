0d6143cbfc252f8c7b37d12f41037392d226b6be
Alex Ramos <alexisramos@gmail.com>
2017-05-14 02:48:25 -0700

    Small change to model.py

--- model.py
+++ model.py
@@ -10,6 +10,7 @@ from keras.layers import Flatten, Dense, Lambda, Cropping2D
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 from sklearn.model_selection import train_test_split
+from keras import backend as K
 
 OFFCENTER_CAMERA_STEERING_CORRECTION = 0.2
 BATCH_SIZE = 32
@@ -50,26 +51,26 @@ def gen_imgs(samples, batch_size):
             yield X_train, y_train
 
 def load_samples(input_dir):
-	headers = ('center_image', 'left_image', 'right_image',
-		'steering', 'throttle', 'brake', 'speed')
-	samples = pd.read_csv(input_dir / 'driving_log.csv', header=None, names=headers)
+    headers = ('center_image', 'left_image', 'right_image',
+        'steering', 'throttle', 'brake', 'speed')
+    samples = pd.read_csv(input_dir / 'driving_log.csv', header=None, names=headers)
 
-	# Training images were generated on Windows machine, fix paths to work on Linux/MacOS machines
-	fix_path = lambda p: input_dir / 'IMG' / PureWindowsPath(p).parts[-1]
+    # Training images were generated on Windows machine, fix paths to work on Linux/MacOS machines
+    fix_path = lambda p: input_dir / 'IMG' / PureWindowsPath(p).parts[-1]
 
-	for col in ['center_image', 'left_image', 'right_image']:
-		samples[col]= samples[col].apply(fix_path)
-	return samples
+    for col in ['center_image', 'left_image', 'right_image']:
+        samples[col]= samples[col].apply(fix_path)
+    return samples
 
 base_input_dir = Path('data')
 data_dirs = ('track1', 'track1_reversed', 'track2', 'track2_reversed')
 samples = pd.concat([load_samples(base_input_dir / p) for p in data_dirs],
-	ignore_index=True)
+    ignore_index=True)
 
 # Drop unneccessary columns and reshape dataframe
 samples.drop(['throttle', 'brake', 'speed'], axis=1, inplace=True)
 samples = pd.melt(samples, id_vars=['steering'],
-	value_vars=['center_image', 'left_image', 'right_image'],
+    value_vars=['center_image', 'left_image', 'right_image'],
     var_name='camera_position', value_name='img_path')
 samples['camera_position'] = samples.camera_position.str.replace('_image', '')
 
@@ -83,26 +84,28 @@ train_samples, validation_samples = train_test_split(samples, test_size=0.2)
 train_generator = gen_imgs(train_samples, batch_size=BATCH_SIZE)
 validation_generator = gen_imgs(validation_samples, batch_size=BATCH_SIZE)
 
-# Define network architecture
-model = Sequential()
-model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
-model.add(Cropping2D(cropping=((70, 25), (0,0))))
-model.add(Convolution2D(24, (5, 5), strides=(2, 2), activation='relu'))
-model.add(Convolution2D(36, (5, 5), strides=(2, 2), activation='relu'))
-model.add(Convolution2D(48, (5, 5), strides=(2, 2), activation='relu'))
-model.add(Convolution2D(64, (3, 3), activation='relu'))
-model.add(Convolution2D(64, (3, 3), activation='relu'))
-model.add(Flatten())
-model.add(Dense(100))
-model.add(Dense(50))
-model.add(Dense(10))
-model.add(Dense(1))
 
-model.compile(loss='mse', optimizer='adam')
+with K.get_session() as sesh:
+    # Define network architecture
+    model = Sequential()
+    model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
+    model.add(Cropping2D(cropping=((70, 25), (0,0))))
+    model.add(Convolution2D(24, (5, 5), strides=(2, 2), activation='relu'))
+    model.add(Convolution2D(36, (5, 5), strides=(2, 2), activation='relu'))
+    model.add(Convolution2D(48, (5, 5), strides=(2, 2), activation='relu'))
+    model.add(Convolution2D(64, (3, 3), activation='relu'))
+    model.add(Convolution2D(64, (3, 3), activation='relu'))
+    model.add(Flatten())
+    model.add(Dense(100))
+    model.add(Dense(50))
+    model.add(Dense(10))
+    model.add(Dense(1))
 
-model.fit_generator(train_generator, np.ceil(len(train_samples) / BATCH_SIZE),
-    validation_data=validation_generator,
-    validation_steps=np.ceil(len(validation_samples) / BATCH_SIZE),
-    epochs=3)
+    model.compile(loss='mse', optimizer='adam')
 
-model.save('model.h5')
+    model.fit_generator(train_generator, np.ceil(len(train_samples) / BATCH_SIZE),
+        validation_data=validation_generator,
+        validation_steps=np.ceil(len(validation_samples) / BATCH_SIZE),
+        epochs=3)
+
+    model.save('model.h5')
